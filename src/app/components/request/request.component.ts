59a54bfd63a10d8bd83785c1bb865428a71cf8de
mevlutdzmn <mevlutdzmn@gmail.com>
2022-11-14 18:48:13 +0300

    mevlut

--- src/app/components/request/request.component.ts
+++ src/app/components/request/request.component.ts
@@ -12,6 +12,7 @@ export class RequestComponent implements OnInit {
   request3: any = { requestId: 3, categoryId: 1, wallet: 'tsrgdakerj', reasonRequest: 'eğitim neden', collectedAid: 231 }
   request4: any = { requestId: 4, categoryId: 1, wallet: '45şakerj', reasonRequest: 'öyle işte neden', collectedAid: 231 }
   request5: any = { requestId: 5, categoryId: 1, wallet: 'tskdckş898j', reasonRequest: 'destek neden', collectedAid: 231 }
+  request6: any = { requestId: 6, categoryId: 1, wallet: 'tskdcsfkş898j', reasonRequest: 'deek neden', collectedAid: 231 }
 
   requests = [this.request1, this.request2, this.request3, this.request4, this.request5]
   constructor() {  }
