f54cbf676324cf076b2656ca553c207a486dff7e
mevlutdzmn <mevlutdzmn@gmail.com>
2022-11-19 20:26:25 +0300

    mevlut

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,7 +9,7 @@ import { TokenModel } from '../models/tokenModel';
 })
 export class AuthService {
 
-  apiUrl = 'https://localhost:44314/api/auth/';
+  apiUrl = 'https://localhost:44326/api/auth/';
   constructor(private httpClient:HttpClient) { }
 
   login(loginModel:LoginModel){
