5a5c1e9df656342eccedc1659c56b0ea41adc3e0
mevlutdzmn <mevlutdzmn@gmail.com>
2022-12-06 16:45:24 +0300

    mevlut

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,62 @@
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+ 
+
+  constructor(
+    private formBuilder:FormBuilder,
+    private toastrService: ToastrService,
+    private router: Router,
+    public authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+  this.createRegisterForm();
+   
+    
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+    });
+  }
+
+  register() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      let registerModel = Object.assign({}, this.registerForm.value);
+      this.authService.register(registerModel).subscribe(
+        (response) => {
+          this.toastrService.info(response.message);
+          //localStorage.setItem('token', response.data.token);
+          this.toastrService.success(response.message, 'Registered.');
+        },
+        (responseError) => {
+        
+          this.toastrService.error(responseError.error);
+        }
+      );
+      this.router.navigate([`/`]);
+    } else {
+      this.toastrService.error('Form geçersiz.');
+    }
+  }
+
+
+  
+}
